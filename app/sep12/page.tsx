a65fea24e7e45cb0754f20c19bb84bd46aadb4b2
Matteo Carroll <matteo@fazendacompany.com>
2026-08-31 21:36:48 -0400

    sep12: note the store is open all day

--- app/sep12/page.tsx
+++ app/sep12/page.tsx
@@ -9,7 +9,7 @@ const HEADLINE = "OPENING DAY EVENT"
 
 const INTRO = [
   "Please join us for the Fazenda opening at 5pm, Sep 12. Food and drinks will be offered.",
-  "Please rsvp below as it is a listed event starting 5pm. We'll be open throughout the day as usual. We hope to see you soon!",
+  "Please rsvp below as it is a listed event starting 5pm. We'll be open throughout the day as usual so you're welcome to stop by earlier as well. We hope to see you soon!",
   "Start your Saturday evening at Fazenda :)",
 ]
 
